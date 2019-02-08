95aaeaae9455a16c48fdb61ef13fdb3304d7a659
kanatamon <ponde.moonlord@gmail.com>
2019-02-08 14:29:23 +0700

    :sparkles: Add lecture of config sourceMap

---
+++ Lecture_02_Compiler/Lesson_02_Option_Source_Map/app.ts
@@ -0,0 +1,8 @@
+/* 
+  Trying to run this code by running command `npm run dev`
+  Then nivigate to DevTools/Sources, we will see app.ts
+  So you can add break point to the ts file
+*/
+
+let a = 'string';
+a = 25;
