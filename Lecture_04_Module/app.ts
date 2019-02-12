e4a8a2b720829a6627d19629e1a745a0bdd61682
DESKTOP-15GUKDC\Kantapon Sritiyot <kantpaon.sritiyot@gmail.com>
2019-02-12 16:47:53 +0700

    :sparkles: Add lecture of module

---
+++ Lecture_04_Module/app.ts
@@ -0,0 +1,5 @@
+import * as Circle from './Math/Circle';
+import * as Rectangle from './Math/Rectangle';
+
+console.log(Circle.calculateCircumference(10));
+console.log(Rectangle.calculateRectangle(10, 20));
